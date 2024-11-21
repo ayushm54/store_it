71624f5013210887c12c5d6a08736bbcb715bd81
Ayush Maheshwari - a0m0snj <Ayush.Maheshwari@walmart.com>
2024-11-21 22:28:10 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/users.actions";
+
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
+  return (
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader accountId={accountId} ownerId={userId} />
+        <form
+          action={async () => {
+            "use server";
+            await signOutUser();
+          }}
+        >
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
